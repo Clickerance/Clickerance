28ced0395846e0bb5a440ef7b27b2bc0148d91ef
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-01-17 15:37:07 +0000

    Added simple heading and subtitle

--- holding-page/src/App.js
+++ holding-page/src/App.js
@@ -3,24 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="Heading">
+                <div className="text">
+                    <h1 className="Heading1">Clickerance</h1>
+                    <h3 className="Heading2">Next-generation shopping.</h3>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
