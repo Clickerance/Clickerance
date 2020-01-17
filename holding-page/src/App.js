de30cde35315eb31f82459230bbc9257d4e0b2ca
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-01-17 15:47:15 +0000

    Made fonts bigger (for greater visual effect).

--- holding-page/src/App.js
+++ holding-page/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div className="Heading">
+            <div className="Header">
                 <div className="text">
                     <h1 className="Heading1">Clickerance</h1>
                     <h3 className="Heading2">Next-generation shopping.</h3>
