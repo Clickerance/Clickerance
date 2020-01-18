a969f6fed98a50b69eee01f1f9573867665bea57
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-01-18 09:49:46 +0000

    Added 404 page.

--- holding-page/src/App.js
+++ holding-page/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useLayoutEffect, useRef} from 'react';
 import './App.css';
 import {TextInput, minorScale, Button, Alert, Textarea, Icon} from "evergreen-ui";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 
 let useWindowSize = () => {
@@ -26,30 +27,45 @@ function App() {
     });
     const contactRef = useRef(null);
     return (
-        <div className="App">
-            <div className="Header" style={{paddingTop: height / 4, paddingBottom: height / 4}}>
-                <div className="text">
-                    <h1 className="Heading1">Clickerance</h1>
-                    <h3 className="Heading2">Next generation shopping.</h3>
-                    <Button onClick={() => {
-                        contactRef.current.scrollIntoView({behavior: 'smooth'})
-                    }} appearance="minimal" marginTop={height / 8} height={minorScale(15)}>
-                        <Icon icon="chevron-down" color="#fff" size={minorScale(15)}/>
-                    </Button>
-                </div>
-            </div>
-            <div className="Contact" ref={contactRef}
-                 style={{paddingLeft: width / 25, paddingBottom: minorScale(10), marginRight: width / 10}}>
-                <h1 className="Heading1">
-                    Contact
-                </h1>
-                <p>To get in touch please email <code style={{display: 'inline'}}>{"santiago"}</code>.<code
-                    style={{display: 'inline'}}>{"schmitt"}</code>@<code
-                    style={{display: 'inline'}}>{"btopenworld"}</code>.<code
-                    style={{display: 'inline'}}>{"com"}</code>.
-                </p>
-            </div>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <div className="App">
+                        <div className="Header" style={{paddingTop: height / 4, paddingBottom: height / 4}}>
+                            <div className="text">
+                                <h1 className="Heading1">Clickerance</h1>
+                                <h3 className="Heading2">Next generation shopping.</h3>
+                                <Button onClick={() => {
+                                    contactRef.current.scrollIntoView({behavior: 'smooth'})
+                                }} appearance="minimal" marginTop={height / 8} height={minorScale(15)}>
+                                    <Icon icon="chevron-down" color="#fff" size={minorScale(15)}/>
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="Contact" ref={contactRef}
+                             style={{paddingLeft: width / 25, paddingBottom: minorScale(10), marginRight: width / 10}}>
+                            <h1 className="Heading1">
+                                Contact
+                            </h1>
+                            <p>To get in touch please email <code style={{display: 'inline'}}>{"santiago"}</code>.<code
+                                style={{display: 'inline'}}>{"schmitt"}</code>@<code
+                                style={{display: 'inline'}}>{"btopenworld"}</code>.<code
+                                style={{display: 'inline'}}>{"com"}</code>.
+                            </p>
+                        </div>
+                    </div>
+                </Route>
+                <Route exact path="/404">
+                    <div className="Header" style={{paddingTop: height / 4, paddingBottom: height / 4}}>
+                        <div className="text">
+                            <h1 className="Heading1">Error 404. Page not found.</h1>
+
+                        </div>
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
+
     );
 }
 
