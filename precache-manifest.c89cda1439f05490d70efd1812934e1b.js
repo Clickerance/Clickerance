6aec57512bee9ced93e12113990278f3582db505
teymour-aldridge <teymour-aldridge@users.noreply.github.com>
2020-01-18 09:06:35 +0000

    Automated deployment: Sat Jan 18 09:06:35 UTC 2020 44393eb4e83e93883bb0a86f5e3c73401b52dacc

---
+++ precache-manifest.c89cda1439f05490d70efd1812934e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4672b6750b299a43c518a3a45204e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "612ee266141fbdee0fad",
+    "url": "/static/css/main.a0725123.chunk.css"
+  },
+  {
+    "revision": "dac8125cd71fb81662f9",
+    "url": "/static/js/2.6429b894.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.6429b894.chunk.js.LICENSE"
+  },
+  {
+    "revision": "612ee266141fbdee0fad",
+    "url": "/static/js/main.f3109796.chunk.js"
+  },
+  {
+    "revision": "7a54b00a1477a7ca7899",
+    "url": "/static/js/runtime-main.0aee4015.js"
+  }
+]);
